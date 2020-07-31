7e15119e6bdb57d764c73fa6c6417684ed2606ae
Mikhail <mb.38@yandex.ru>
2020-07-31 08:56:33 +0700

    commit 1

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,3 +1,6 @@
+import {authAPI} from "../api/api";
+import {stopSubmit} from "redux-form";
+
 const SET_USER_DATA = 'SET_USER_DATA';
 
 let initialState = {
@@ -7,13 +10,12 @@ let initialState = {
     isAuth: false,
 }
 
-const authReducer = (state = initialState, action) => {
+const appReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_DATA:{
             return {
                 ...state,
-                ...action.data,
-                isAuth: true,
+                ...action.payload
             }
         }
         default:
@@ -21,6 +23,35 @@ const authReducer = (state = initialState, action) => {
     }
 }
 
-export const setAuthUserData = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}});
+export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload: {userId, email, login, isAuth}});
 
-export default authReducer;
+export const getAuthUserData = () => (dispatch) => {
+    authAPI.me().then(response => {
+        if (response.data.resultCode === 0) {
+            let {id, email, login} = response.data.data;
+            dispatch(setAuthUserData(id, email, login, true));
+        }
+    })
+}
+
+export const login = (email, password, rememberMe) => (dispatch) => {
+
+    authAPI.login(email, password, rememberMe).then(response => {
+        if (response.data.resultCode === 0) {
+            dispatch(getAuthUserData());
+        } else {
+            let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+            dispatch(stopSubmit('login', {_error: message}));
+        }
+    })
+}
+
+export const logout = () => (dispatch) => {
+    authAPI.logout().then(response => {
+        if (response.data.resultCode === 0) {
+            dispatch(getAuthUserData(null, null, null, false));
+        }
+    })
+}
+
+export default appReducer;
